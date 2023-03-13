63f69fe0ad72a296f306cc58117f33627f254e15
Shivareddykyadari <kyadarishivareddy1430@gmail.com>
2023-03-13 22:37:04 +0530

    day3

--- js/index.js
+++ js/index.js
@@ -11,3 +11,27 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link")
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+var sidemenu=document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right="0"
+}
+function closemenu(){
+    sidemenu.style.right="-200px"
+}
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "kyadarishivareddy1430@gmail.com",
+        Password : "8869FC2989A861D2EF5D2ED762CA6508076D",
+        To : 'kyadarishivareddy1430@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "subject",
+        Body : "Name: " +document.getElementById("Name").value
+                
+    }).then(
+      message => alert("message sent successfully")
+    );
+}
+
